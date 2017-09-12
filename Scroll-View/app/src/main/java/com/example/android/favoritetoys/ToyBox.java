6a88269282bdf54f1a02461c26a1950d2a9f1969
crissyg <crissyg@users.noreply.github.com>
2017-09-11 22:41:43 -0400

    Edited String Array

---
+++ Scroll-View/app/src/main/java/com/example/android/favoritetoys/ToyBox.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//Edited 09/11/2017 by Christina Gordon
+
+package com.example.android.favoritetoys;
+
+public final class ToyBox {
+
+    /**
+     * This method returns a list of popular toy names from the 20th and early 21st centuries.
+     * I don't know about you guys, but this definitely brings me back to my childhood...
+     *
+     * @return A list of popular toys
+     */
+    public static String[] getToyNames() {
+        return new String[] {
+                "Teddy Bear",
+                "Barbie",
+                "Checkers",
+                "Chess",
+                "Football",
+                "Muppet Toy",
+                "Slinky",
+                "Tennis Ball",
+                "Softball",
+                "Kite",
+                "Pin Wheel",
+                "Legos",
+                "Furry Doll",
+                "Fake Punching Bag",
+                "Skateboard",
+                "Bicycle",
+                "Mini Chalkboard",
+                "Fairy Toy",
+                "Fake Fart",
+                "Mini Tea Set",
+                "Toy Truck",
+                "Toy cars",
+                "Sling Shot",
+                "Play Dough",
+                "Princess Doll",
+                "Puzzle",
+                "Gameboy",
+                "Scrabble",
+                "Mermaid Doll",
+                "Trampoline"
+        };
+    }
+}
